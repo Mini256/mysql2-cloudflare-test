b6245b23b8fb0235bd19dc952789854b639bec84
Mini256 <minianter@foxmail.com>
2023-11-17 17:44:38 +0800

    remove mysql-cloudflare

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@
  *
  * Learn more at https://developers.cloudflare.com/workers/
  */
-import { Connection, createConnection } from 'mysql2/promise';
+import { Connection, createConnection } from 'mysql2';
 
 export interface Env {
 	DATABASE_HOST: string;
@@ -20,18 +20,18 @@ export interface Env {
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
 		try {
-			const tidb = await createConnection({
-				host: '127.0.0.1',
+			const tidb = createConnection({
+				host: env.DATABASE_HOST,
 				port: 4000,
-				user: 'root',
-				password: '',
-				database: 'test',
+				user: env.DATABASE_USER,
+				password: env.DATABASE_PASSWORD,
+				database: env.DATABASE_NAME,
 				debug: true,
 				useStaticParser: true,
 			});
 			// @ts-ignore
-			const [rows] = await tidb.query('SELECT 1 AS field;');
-			await tidb.end()
+			const [rows] = await query(tidb, 'SELECT 1 AS field;');
+			tidb.end();
 			return new Response(JSON.stringify(rows), { status: 200 });
 		} catch (e: any) {
 			console.error(e);
@@ -39,16 +39,16 @@ export default {
 		}
 	},
 };
-//
-// function query(conn: Connection, sql: string) {
-// 	return new Promise((resolve, reject) => {
-// 		conn.query(sql, (err, rows) => {
-// 			if (err) {
-// 				reject(err);
-// 			} else {
-// 				resolve([rows]);
-// 			}
-// 		});
-// 	});
-// }
+
+function query(conn: Connection, sql: string) {
+	return new Promise((resolve, reject) => {
+		conn.query(sql, (err, rows) => {
+			if (err) {
+				reject(err);
+			} else {
+				resolve([rows]);
+			}
+		});
+	});
+}
 
